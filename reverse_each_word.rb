68029dacd5a9e021de79d9170c7ea2966439fd7a
Destiny Boling <bolingdestinyr@gmail.com>
2021-01-27 06:25:28 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,8 +2,7 @@
   def reverse_each_word(string)
   string_to_array = string.split (" ")
   string_to_array.collect { |element|
-    element.reverse!}.join 
+    element.reverse!}.join
   end
   reverse_each_word
   end
-end
